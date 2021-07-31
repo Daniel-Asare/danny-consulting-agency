a53ea050a789f6b535fd109d62f68d3b075132f9
Daniel-Asare <johndoe@example.com>
2021-07-31 17:09:04 +0000

    k

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contact from 'components/pages/Contact';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Router>
     <Navbar/>
     <PageLayout>
@@ -21,7 +21,7 @@ function App() {
     </PageLayout>
     <Footer/>
     </Router>
-    </>
+    </div>
   );
 }
 
